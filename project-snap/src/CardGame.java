428eac2ee18d9938871a362637652238a72bd2fd
Edgars <Edgars.Kaufmanis@nology.io>
2023-01-25 15:18:10 +0000

    first changes

---
+++ project-snap/src/CardGame.java
@@ -0,0 +1,19 @@
+import cards.Card;
+
+import java.util.ArrayList;
+
+public class CardGame {
+    private ArrayList<Card> deckOfCards = new ArrayList<>();
+    private String name;
+
+    public CardGame(ArrayList<Card> deckOfCards, String name) {
+        this.deckOfCards = deckOfCards;
+        this.name = name;
+    }
+
+
+
+    public void getDeck() {
+
+    }
+}
---
+++ project-snap/src/cards/Card.java
@@ -0,0 +1,18 @@
+package cards;
+
+public class Card {
+    private String suit;
+    private String symbol;
+    private int value;
+
+    public Card(String suit, String symbol, int value) {
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return symbol + " of " + suit + " with a value of " + value;
+    }
+}
